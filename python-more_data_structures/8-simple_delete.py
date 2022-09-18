fbee5650cc154af8af4021c795346405e0f36ef3
rrtyui <marcelo6549@gmail.com>
2022-09-18 20:44:34 -0300

    task

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
+
 def simple_delete(a_dictionary, key=""):
     if a_dictionary.get(key) is None:
         return a_dictionary
     else:
         pop = a_dictionary
         pop.pop(key)
-        return pop
+        return pop  
