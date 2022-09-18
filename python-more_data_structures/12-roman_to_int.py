3f28e372b9e233948cb86594ff725af91e811df2
rrtyui <marcelo6549@gmail.com>
2022-09-18 20:52:47 -0300

    fix

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -5,7 +5,8 @@ def roman_to_int(roman_string):
     if roman_string is None or type(roman_string) != str:
         return (0)
     reset = 0
-    dictonary = {'I': 1, 'V': 5, 'X': 10, 'L': 50,'C': 100, 'D': 500, 'M': 1000}
+    dictonary = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                 'C': 100, 'D': 500, 'M': 1000}
     time = ()
     for i in roman_string:
         if len(time) == 0:
