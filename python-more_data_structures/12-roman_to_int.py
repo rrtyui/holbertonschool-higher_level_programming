b5623ad93b9c845efa49a9f0990abed6acdb0009
rrtyui <marcelo6549@gmail.com>
2022-09-18 20:51:05 -0300

    fix

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -6,7 +6,7 @@ def roman_to_int(roman_string):
         return (0)
     reset = 0
     dictonary = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
- 'C': 100, 'D': 500, 'M': 1000}
+    'C': 100, 'D': 500, 'M': 1000}
     time = ()
     for i in roman_string:
         if len(time) == 0:
